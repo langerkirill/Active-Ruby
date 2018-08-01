66a16bf396aafe2b7d0ffe128794d7e5caeb6a0f
Kirill <langerkirill@gmail.com>
2018-07-31 20:26:50 -0400

    completed the has one through association

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -1,10 +1,18 @@
 require_relative '03_associatable'
 
-# Phase IV
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    options = {}
+    options[name] = BelongsToOptions.new(name)
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options =
+      through_options.model_class.assoc_options[source_name]
+      through_key = self.send(through_options.foreign_key)
+      source_options
+              .model_class
+              .where(source_options.primary_key => through_key).first
+    end
   end
 end
